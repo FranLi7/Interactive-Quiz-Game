7ac34c9c4ee62f6f5585670d6a94c8e922c21ef1
FranLi7 <franciscolimon1200@gmail.com>
2024-03-08 12:25:33 -0800

    Add JavaScript quiz questions and answers

---
+++ script.js
@@ -0,0 +1,70 @@
+const questions = [
+  {
+    question:
+      'Which of the following is a correct variable declaration in JavaScript?',
+    choices: ['let myVar', 'variable myVar', 'v myVar', 'declare myVar'],
+    answer: 1,
+  },
+  {
+    question: 'How do you create a function in JavaScript?',
+    choices: [
+      'function:myFunction()',
+      'function=myFunction()',
+      'function myFunction()',
+      'create myFunction()',
+    ],
+    answer: 3,
+  },
+  {
+    question: 'How do you write an IF statement in JavaScript?',
+    choices: ['if i == 5 then', 'if i = 5 then', 'if (i == 5)', 'if i = 5'],
+    answer: 3,
+  },
+  {
+    question: 'How can you add a comment in JavaScript?',
+    choices: [
+      '//This is a comment',
+      '<!--This is a comment-->',
+      "'This is a comment",
+      '(This is a comment)',
+    ],
+    answer: 1,
+  },
+  {
+    question: 'What is the correct way to write a JavaScript array?',
+    choices: [
+      "let colors = 'red', 'green', 'blue'",
+      "let colors = ['red', 'green', 'blue']",
+      "let colors = (1:'red', 2:'green', 3:'blue')",
+      "let colors = 1=('red'), 2=('green'), 3=('blue')",
+    ],
+    answer: 2,
+  },
+  {
+    question: 'How do you round the number 7.25 to the nearest integer?',
+    choices: ['Math.rnd(7.25)', 'rnd(7.25)', 'Math.round(7.25)', 'Round(7.25)'],
+    answer: 3,
+  },
+  {
+    question: 'Which event occurs when a user clicks on an HTML element?',
+    choices: ['onmouseover', 'onclick', 'onchange', 'onmouseclick'],
+    answer: 2,
+  },
+  {
+    question:
+      'How do you find the number with the highest value of x and y using JavaScript?',
+    choices: ['Math.ceil(x, y)', 'Math.max(x, y)', 'ceil(x, y)', 'max(x, y)'],
+    answer: 2,
+  },
+  {
+    question:
+      'Which operator is used to assign a value to a variable in JavaScript?',
+    choices: ['X', '-', '=', '+'],
+    answer: 3,
+  },
+  {
+    question: 'What will the following code return: Boolean(10 > 9)?',
+    choices: ['NaN', 'false', 'true', 'undefined'],
+    answer: 3,
+  },
+];
